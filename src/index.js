7f259e55348708936ff20c18fb5fc121861d65b3
Ivan Mrvelj <sk8raid@gmail.com>
2016-11-06 16:56:25 +0100

    chore: update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
+// Math.random() * (max - min) + min;
+
 import moment from 'moment';
 
-function randomMoment() {
-    // Computes range
-    // Math.random() * (max - min) + min;
-
-    var today = moment().unix();
+const randomMoment = () => {
+    const today = moment().unix();
     return moment.unix(Math.random() * today);
-}
-
-module.exports = function() {
-    return new randomMoment();
 };
+
+module.exports = randomMoment;
