e28e95aa3ca80f423141c3e3ab239b795fc1b7be
pin37 <ucorpor@gmail.com>
2018-01-31 22:27:36 +0400

    fix milliseconds generation

--- src/index.js
+++ src/index.js
@@ -8,19 +8,18 @@ const moment = require('moment');
  * @returns 
  */
 function momentRandom(end = moment(), start) {
-  const endMoment = moment(end);
+  const endTime = +moment(end);
   const randomNumber = (to, from = 0) =>
     Math.floor(Math.random() * (to - from) + from);
 
   if (start) {
-    const startMoment = moment(start);
-    if (startMoment.unix() > endMoment.unix()) {
+    const startTime = +moment(start);
+    if (startTime > endTime) {
       throw new Error('End date is before start date!');
     }
-    return moment.unix(randomNumber(endMoment.unix(), startMoment.unix()));
-  } else {
-    return moment.unix(randomNumber(endMoment.unix()));
+    return moment(randomNumber(endTime, startTime));
   }
+  return moment(randomNumber(endTime));
 }
 
 module.exports = momentRandom;
